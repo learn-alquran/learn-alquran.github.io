85db35a09e6798b239e14911c23094e215d435b6
Al Asaad <al-ahmadgaid.asaad@aboitiz.com>
2019-12-29 08:46:05 +0800

    added scripts

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
-document.addEventListener("DOMContentLoaded", event => {
-    console.log("hellow")
+document.addEventListener(`DOMContentLoaded`, event => {
+    console.log(`hellow`)
     const app = firebase.app();
     const db  = firebase.firestore();
-    const surah = db.collection("al-fatihah");
+    const surah = db.collection(`al-fatihah`);
     console.log(app)
     
-    surah.doc("1-2").set({
-        verse_no : "2",
-        verse  : " ٱللَّهُ ٱلصَّمَدُ"
+    surah.doc(`1-2`).set({
+        verse_no : `2`,
+        verse  : ` ٱللَّهُ ٱلصَّمَدُ`
     })
 
-    surah.doc("1-3").set({
-        verse_no : "3",
-        verse  : " لَمْ يَلِدْ وَلَمْ يُولَدْ"
+    surah.doc(`1-3`).set({
+        verse_no : `3`,
+        verse  : ` لَمْ يَلِدْ وَلَمْ يُولَدْ`
     })
 
-    surah.doc("1-4").set({
-        verse_no : "4",
-        verse  : " وَلَمْ يَكُن لَّهُۥ كُفُوًا أَحَدٌۢ"
+    surah.doc(`1-4`).set({
+        verse_no : `4`,
+        verse  : ` وَلَمْ يَكُن لَّهُۥ كُفُوًا أَحَدٌۢ`
     })
 
-    surah.doc("1-1").get()
+    surah.doc(`1-1`).get()
         .then(doc => {
             const data = doc.data();
             document.write(data.verse_no + `<br>`)
